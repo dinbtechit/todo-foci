e44f659f60cecb5e9b56c47b549224ec9652930c
Dinesh Srini <dinbtechit@gmail.com>
2025-02-25 16:57:24 -0400

    Sorting Mechanism

--- src/components/todo/components/sort-todo.tsx
+++ src/components/todo/components/sort-todo.tsx
@@ -1,10 +1,46 @@
+'use client';
 import {Button} from "@/components/ui/button";
 import {ListFilterPlus} from "lucide-react";
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import {useState} from "react";
 
 export default function SortTodo() {
+    const [groupByDates, setGroupByDates] = useState(true)
+    const [sortByDates, setSortByDates] = useState('date_asc')
     return (
-        <Button variant={'secondary'} className="text-white">
-            <ListFilterPlus className="text-white"/>
-        </Button>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button className="bg-black/95 hover:bg-gray-800/90">
+                    <ListFilterPlus className="text-white"/>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel>Filter Settings</DropdownMenuLabel>
+                <DropdownMenuSeparator/>
+                <DropdownMenuCheckboxItem checked={groupByDates}
+                                          onSelect={() => setGroupByDates(!groupByDates)}>
+                    Group By Dates
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuSeparator/>
+                <DropdownMenuRadioGroup value={sortByDates} onValueChange={setSortByDates}>
+                    <DropdownMenuLabel>Sort By Dates</DropdownMenuLabel>
+                    <DropdownMenuRadioItem value="date_asc">Ascending</DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="date_desc">Descending</DropdownMenuRadioItem>
+                    <DropdownMenuSeparator/>
+                    <DropdownMenuLabel>Sort By Title</DropdownMenuLabel>
+                    <DropdownMenuRadioItem value="title_asc">Ascending</DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="title_desc">Descending</DropdownMenuRadioItem>
+                </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
     )
 }
