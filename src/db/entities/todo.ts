e9494b2e614c97e78586355a503be2741ef40734
Dinesh Srini <dinbtechit@gmail.com>
2025-03-08 17:15:16 -0400

    without timezone test

--- src/db/entities/todo.ts
+++ src/db/entities/todo.ts
@@ -13,13 +13,13 @@ export class Todo {
     @Column({nullable: true})
     description: string;
 
-    @Column({type: 'timestamptz'})
+    @Column({type: 'timestamp without time zone'})
     dueDate: Date;
 
     @Column({default: false})
     completed: boolean;
 
-    @Column({type: 'timestamptz'})
+    @Column({type: 'timestamp without time zone'})
     createdAt: Date;
 
     @ManyToOne(() => User, (user) => user.id)
